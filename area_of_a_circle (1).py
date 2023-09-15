8c89733d18eb1ac9565e941c3ff34ab988c33bfb
RiyaP49 <145067415+RiyaP49@users.noreply.github.com>
2023-09-14 23:48:26 -0500

    Add files via upload

---
+++ area_of_a_circle (1).py
@@ -0,0 +1,20 @@
+def compute_area_of_circle(radius):
+	pi = 3.14
+	area = pi * radius * radius
+	return area
+
+radius1 = 30
+area1 = compute_area_of_circle(radius1)
+print(f"The area of circle with radius {radius1} is: {area1}")
+
+radius2 = 40
+area2 = compute_area_of_circle(radius2)
+print(f"The area of circle with radius {radius2} is: {area2}")
+
+radius3 = 55
+area2 = compute_area_of_circle(radius2)
+print(f"The area of circle with radius {radius2} is: {area2}")
+
+print("What changes were made?")
+
+main()
